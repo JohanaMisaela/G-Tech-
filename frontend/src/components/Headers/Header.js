2ead7616035351ce4d4c46b27ba52a6077dc703f
Johanna Misaela <116675181+JohanaMisaela@users.noreply.github.com>
2022-10-28 22:38:55 +0300

    React et Node js
    Projet 210

---
+++ frontend/src/components/Headers/Header.js
@@ -0,0 +1,70 @@
+/*!
+
+=========================================================
+* Argon Dashboard React - v1.2.2
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-dashboard-react
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+
+// reactstrap components
+import { Card, CardBody, CardTitle, Container, Row, Col } from "reactstrap";
+import Gtech from '../../assets/img/brand/argon-react.png'
+import Esti from '../../assets/img/brand/esti.png'
+import Mlay from '../../assets/img/brand/mlay.png'
+import G from '../../assets/img/brand/g.png'
+
+const Header = () => {
+  return (
+    <>
+      <div className="header bg-gradient-info pb-8 pt-5 pt-md-8">
+        <Container fluid>
+          <div className="header-body">
+            {/* Card stats */}
+            <Row>
+              <Col lg="6" xl="3">
+                <Card className="card-stats mb-4 mb-xl-0">
+                  <CardBody>
+                    <img src={Gtech} width="90%" height="110px"  ></img>
+                  </CardBody>
+                </Card>
+              </Col>
+              <Col lg="6" xl="3">
+                <Card className="card-stats mb-4 mb-xl-0">
+                  <CardBody>
+                  <img src={Esti} width="90%"  height="110px"></img>
+                  </CardBody>
+                </Card>
+              </Col>
+              <Col lg="6" xl="3">
+                <Card className="card-stats mb-4 mb-xl-0">
+                  <CardBody>
+                  <img src={Mlay} width="100%"  height="110px"></img>
+                  </CardBody>
+                </Card>
+              </Col>
+              <Col lg="6" xl="3">
+                <Card className="card-stats mb-4 mb-xl-0">
+                  <CardBody>
+                  <img src={G} width="90%"  height="110px"></img>
+                  </CardBody>
+                </Card>
+              </Col>
+            </Row>
+          </div>
+        </Container>
+      </div>
+    </>
+  );
+};
+
+export default Header;
