297aae5da161234c66d7d9a586a38dad4c05837c
Johanna Misaela <116675181+JohanaMisaela@users.noreply.github.com>
2022-10-28 22:44:03 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -32,6 +32,7 @@ app.use((err, req, res, next) => {
 
 
 //const PORT = process.env.PORT || 4000;
+//you can change your port as the same from env 
 app.listen(process.env.PORT || 4000, () => {
   console.log('Server is listening on port 4000');
 });
